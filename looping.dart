bea744d9d21ed6ac41d7448914f0e3c3efb20474
Md Noor Alam <nour.buet@gmail.com>
2024-02-20 13:57:23 -0800

    Second comment

--- looping.dart
+++ looping.dart
@@ -1,4 +1,21 @@
+import 'dart:io';
+
 void main() {
+  List<String> studentlist=['Rifat','Soeb','Masud'];
+  print('Student List (directi print): $studentlist');
+
+  for (int i=0;i<studentlist.length;i++){
+    // Vertical printing
+    print('Student List (vertical): $i=> ${studentlist[i]}');
+    // stdout.write('Student List: $i=> ${studentlist[i]}');
+  }
+
+
+  for (int i=0;i<studentlist.length;i++){
+    // Horizontal printing
+    stdout.write('Student List: $i=> ${studentlist[i]}');
+  }
+
   Map<int, String> studentmap = {
     1: 'Rafi',
     2: 'Saklain',
@@ -11,9 +28,9 @@ void main() {
   // for (int i=0;i<studentmap.length;i++){
   //   print('Student Mapping: $i=>${studentmap}');
   // }
-for(int i in studentmap.keys) {
-  for (String item in studentmap.values) {
-    print('Student Mapping:$i: $item');
-  }
-}
+// for(int i in studentmap.keys) {
+//   for (String item in studentmap.values) {
+//     print('Student Mapping:$i: $item');
+//   }
+// }
 }
