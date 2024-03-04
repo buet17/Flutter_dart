ec0452c3058b8013647853b3e474ca62193f1b8b
Md Noor Alam <nour.buet@gmail.com>
2024-03-03 19:19:56 -0800

    OOP again

--- oop.dart
+++ oop.dart
@@ -3,22 +3,24 @@ class Bicycle{
   int? size;
   int? currentSpeed;
   static int? rpm;
-  void changeGear(int newValue){
-    currentSpeed=newValue;
+
+  static double disPrice(double price){
+    return price;
 }
-    Bicycle(String color, int size, int currentSpeed){
-      this.color=color;
-      this.size=size;
-      this.currentSpeed=currentSpeed;
+    Bicycle(this.color, this.size, this.currentSpeed)
+    {
       rpm=7;
     }
 
+  void changeGear(int newValue){
+    currentSpeed=newValue;
+  }
 
   void display(){
     print("Color of the Bicycle is: $color");
     print("size of the Bicycle is: $size");
     print("Current Speed of the Bicycle is: $currentSpeed");
-    print("rpm of the Bicycle is: $rpm");
+    // print("rpm of the Bicycle is: ${Bicycle.rpm}");
   }
 }
 
@@ -29,4 +31,6 @@ void main(){
   // bicycle.currentSpeed=0;
   bicycle.changeGear(5);
   bicycle.display();
+  print("rpm of the Bicycle is: ${Bicycle.rpm}");
+  print("Price of the Bicycle is: ${Bicycle.disPrice(500)}");
 }
