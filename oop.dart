696ca190034cb6e642be07ea3607cb858b1bf74e
Md Noor Alam <nour.buet@gmail.com>
2024-03-03 19:59:54 -0800

    todays last oop

--- oop.dart
+++ oop.dart
@@ -1,36 +1,49 @@
 class Bicycle{
   String? color;
-  int? size;
+  int? _size;
   int? currentSpeed;
   static int? rpm;
 
   static double disPrice(double price){
     return price;
 }
-    Bicycle(this.color, this.size, this.currentSpeed)
+    Bicycle(this.color, this._size, this.currentSpeed)
     {
       rpm=7;
+      print("Price of the Bicycle is: ${Bicycle.disPrice(500)}");
     }
 
   void changeGear(int newValue){
     currentSpeed=newValue;
   }
-
-  void display(){
+  void _display(){
     print("Color of the Bicycle is: $color");
-    print("size of the Bicycle is: $size");
+    print("size of the Bicycle is: $_size");
     print("Current Speed of the Bicycle is: $currentSpeed");
     // print("rpm of the Bicycle is: ${Bicycle.rpm}");
   }
 }
 
+class Cyclebd extends Bicycle{
+  @override
+  void _display(){
+    print("Excellent quality");
+    print("Color of the Bicycle is: $color");
+    print("size of the Bicycle is: $_size");
+    print("Current Speed of the Bicycle is: $currentSpeed");
+  }
+  Cyclebd(super.color, super._size, super.currentSpeed);
+}
+
 void main(){
   Bicycle bicycle=Bicycle('Red',26,0);
   // bicycle.color='Red';
-  // bicycle.size=26;
   // bicycle.currentSpeed=0;
   bicycle.changeGear(5);
-  bicycle.display();
+  bicycle._display();
   print("rpm of the Bicycle is: ${Bicycle.rpm}");
-  print("Price of the Bicycle is: ${Bicycle.disPrice(500)}");
+  print("Price of the Bicycle is: ${Bicycle.disPrice(700)}");
+
+  Cyclebd cbd=Cyclebd('Green',34,0);
+  cbd._display();
 }
